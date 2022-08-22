86fe17ee1b05057c41907a021b4185620dbfd44c
Adalyne Cummins <adalynecummins@gmail.com>
2022-08-22 15:06:17 +0100

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  resources :products do
+    resources :purchases, only: [:create, :new,]
+  end
 end
