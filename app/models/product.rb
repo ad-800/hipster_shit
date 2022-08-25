23140f8dc0a1819eeb88ceff5788dc6d09b25316
Michele Scotellaro <scotellaro.michele@gmail.com>
2022-08-25 09:57:35 +0100

    search v1

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,11 @@
 class Product < ApplicationRecord
   has_one_attached :photo
   has_one :purchase
+
+  include PgSearch::Model
+  pg_search_scope :search_by_title_and_description,
+  against: [ :title, :description ],
+  using: {
+    tsearch: { prefix: true }
+  }
 end
